92be10b6bb7f94886b0c643667b1e040ef84beed
czajemi <e.czajaaa@gmail.com>
2022-01-02 19:47:40 +0100

    Create Game class

---
+++ src/com/company/Game.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Game {
+    // create variables used by Game class
+    private int wins, losses, pushes;
+
+    // constructor for the Game class
+    public Game(){
+        wins = 0;
+        losses = 0;
+        pushes = 0;
+    }
+}
